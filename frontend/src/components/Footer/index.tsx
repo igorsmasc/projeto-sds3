89b34b013359a514aa36e7a9016bd73800149fb6
igorsmasc <igorsmascarenhas@outlook.com>
2021-05-03 23:31:02 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App developed by{" "}
+          <a
+            href="https://github.com/igorsmasc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Igor Mascarenhas
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Event promoted by DevSuperior school:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
